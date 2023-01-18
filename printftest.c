225bb757a908e083238f8c23d91a5875961e359b
211300027-zhao jinwei <487394112@qq.com>
2023-01-18 19:43:53 +0800

    create func in printftest.c

--- printftest.c
+++ printftest.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
+int myvsprintf()
+{
+  return 0;
+}
+
+int mysprintf()
+{
+  return 0;
+}
+
+int myprintf()
+{
+  return 0;
+}
+
 int main(){
+  int a=myprintf();
+  int b=mysprintf();
+  int c=myvsprintf();
+  int d=a+b+c;
+  printf("%d\n",d);
   return 0;
 }
